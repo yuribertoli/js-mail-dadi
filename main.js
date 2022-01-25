a34303a118249464cead16226301294b14b9bf1a
yuribertoli <yuribertoli@yahoo.it>
2022-01-25 15:32:34 +0100

    controllo mail

--- main.js
+++ main.js
@@ -2,33 +2,56 @@ let domanda = prompt("Qual'è la tua mail?");
 
 const mails = ["pino@gmail.com","aldo@gmail.com","gino@gmail.com"];
 
+let controllo = false;
+let controllo2 = false;
+
 for (let i=0; i<mails.length; i++) {
 
     if (mails[i] == domanda) {
+
         alert("La mail inserita " + mails[i] + " è presente nel sistema, puoi accedere al sito e giocare a dadi");
+        controllo = true;
+        break;  
     } 
 
 }
 
-/*  else {
+if (controllo != true) {
 
-    alert("Non puoi accedere al sito. Registra la tua mail e riprova"); */
+    alert("Non puoi accedere al sito. Registra la tua mail e riprova");
 
-    /* let aggiungiMail = prompt("Scrivi la tua mail per registrarti");
+    let aggiungiMail = prompt("Scrivi la tua mail per registrarti");
 
     mails.push(aggiungiMail);
 
     alert("Email correttamente aggiunta");
 
     let domanda = prompt("Riprova inserendo la tua mail adesso");
+
+    for (let i=0; i<mails.length; i++) {
+
+        if (mails[i] == domanda) {
     
-    if (mails[i] == domanda) {
-        alert("Ora sei registrato e puoi accedere al sito");
-    }  else {
+            alert("La mail inserita " + mails[i] + " è ora presente nel sistema, puoi accedere al sito e giocare a dadi");
+            controllo2 = true;            
+            break;
+
+        } 
+        
+    }
+    
+    if (controllo2 != true) {
         alert("Le email che hai inserito non combaciano. Riaggiorna la pagina e riprova");
-    } 
+    }
+
 }
-*/
+
+
+
+
+
+
+
 
 let dado = document.querySelector("button");
 
